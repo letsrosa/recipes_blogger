85615abfdb258d297169edbddcbc3f17f8452579
letsrosa <leticiarosasouza6304@gmail.com>
2025-08-27 21:23:05 -0300

    criacao desse site de receitas para armazenamento e organizacao.

---
+++ receitas/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,  get_object_or_404
+from .models import Receita
+
+# Create your views here.
+def home(request):
+    return render(request, 'receitas/home.html')
+
+def receita_detail(request, receita_id):
+    receita = get_object_or_404(Receita, pk=id) 
+
+    context = {
+        'receita': receita,   
+        }
+
+    return render(request, 'receitas/receita_detail.html', context)
