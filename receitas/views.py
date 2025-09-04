597af090ee5b2d15d735e46489211f37e84fb7eb
letsrosa <leticiarosasouza6304@gmail.com>
2025-09-04 19:39:42 -0300

    ajustes no search

--- receitas/views.py
+++ receitas/views.py
@@ -3,26 +3,22 @@ from .models import Receita
 
 # Create your views here.
 def home(request):
-    return render(request, 'receitas/home.html')
+    receitas = Receita.objects.all()
+    return render(request, 'receitas/home.html', {'receitas': receitas})
 
 def receita_detail(request, id):
     receita = get_object_or_404(Receita, pk=id) 
-
-    context = {
-        'receita': receita,   
-        }
-
-    return render(request, 'receitas/receita_detail.html', context)
+    return render(request, 'receitas/receita_detail.html', {'receita': receita})
 
 def pesquisar_receitas(request):
-    query = request.GET.get('q') # pega o que foi digitado no campo de busca
+    query = request.GET.get('q',"") # pega o que foi digitado no campo de busca
     resultados = []
     
     if query:
     # filtra receitas que contenham o termo no nome (sem casesensitive)
         resultados = Receita.objects.filter(title__icontains=query)
-    context = {
-    'query': query,
-    'resultados': resultados,
-    }
-    return render(request, 'receitas/pesquisa.html', context)
+    return render(request, 'receitas/pesquisa.html', {
+        'query': query, 
+        'resultados': resultados
+    })
+    
